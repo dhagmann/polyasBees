b2353f282214a2cc82f98a1a9a09d2d7117f69cf
dhagmann <hagmann@cmu.edu>
2014-02-25 21:52:14 -0500

    Merged code
    Now runs all simulations and writes results of individual run to file.
    
    Renamed probPop to disruptionRate.

--- Bees and Urns/polyaBees_discovery.py
+++ Bees and Urns/polyaBees_discovery.py
@@ -1,62 +1,119 @@
 from random import random, randint, uniform
 from joblib import Parallel, delayed
 import itertools, win32api, win32process, win32con
+import csv, time
+import lockfile
 
 repetitions = 100000
-simType = "discovery"
+simType = "numTypes"
 
+if simType == "numTypes":
+    globalBalls = ((1, 1), (1, 1, 1), (1, 1, 1, 1))
+    globalIncrease = (((2,),(1,), (1,), (1,)),)
+    disruptionRate = [0,]
+    discoveryRate = [0,]
+    discoveryRateOptimal = [0,]
+    recruitmentRate = [1,]
+if simType == "ratios":
+    globalBalls = ((1, 1, 1),)
+    globalIncrease = (((3,),(1,), (1,)), ((3,),(2,), (1,)))
+    disruptionRate = [0,]
+    discoveryRate = [0,]
+    discoveryRateOptimal = [0,]
+    recruitmentRate = [1,]
+if simType == "noise":
+    globalBalls = ((1, 1),)
+    globalIncrease = (((3, 2, 2, 1),(2, 1, 1, 0)), ((2, 2, 2, 2),(1, 1, 1, 1)))
+    disruptionRate = [0,]
+    discoveryRate = [0,]
+    discoveryRateOptimal = [0,]
+    recruitmentRate = [1,]
+if simType == "multiAttributesFixed":
+    globalBalls = ((1, 1),)
+    globalIncrease = (((3, 1), (1, 1)),((2, 2), (1, 1)))
+    disruptionRate = [0,]
+    discoveryRate = [0,]
+    discoveryRateOptimal = [0,]
+    recruitmentRate = [1,]
+if simType == "multiAttributesDirectComparison":
+    globalBalls = ((1, 1),)
+    globalIncrease = (((3, 1), (2, 2)),)
+    disruptionRate = [0,]
+    discoveryRate = [0,]
+    discoveryRateOptimal = [0,]
+    recruitmentRate = [1,]
+if simType == "infoCascade":
+    globalBalls = ((1, 3),)
+    globalIncrease = (((2,), (1,)),)
+    disruptionRate = [0, 0.05, 0.10, 0.20]
+    discoveryRate = [0,]
+    discoveryRateOptimal = [0,]
+    recruitmentRate = [1,]
+if simType == "varyingSuboptimalPayoff":
+    globalBalls = ((1, 1),)
+    globalIncrease = (((4,), (1,)), ((4,), (2,)), ((4,), (3,)))
+    disruptionRate = [0,]
+    discoveryRate = [0,]
+    discoveryRateOptimal = [0,]
+    recruitmentRate = [1,]
+if simType == "varyingOptimalPayoff":
+    globalBalls = ((1, 1),)
+    globalIncrease = (((2,), (1,)), ((3,), (1,)), ((4,), (1,)))
+    disruptionRate = [0,]
+    discoveryRate = [0,]
+    discoveryRateOptimal = [0,]
+    recruitmentRate = [1,]
 if simType == "discovery":
     globalBalls = ((0, 0),)
-    globalIncrease = (((2,),(1,),),((100,),(1,),))
-    probPop = [0, 0.1, 0.5, 1]
-    discoveryRate = [0.5,0.75]
+    globalIncrease = (((2,),(1,),),)
+    disruptionRate = [0.5]
+    discoveryRate = [0.25,0.75]
     discoveryRateOptimal = [0.1,0.75]
-    recruitmentRate = [0.5]
+    recruitmentRate = [0.5,]
 
-outputFile = simType + "discoveryFebruary2014.csv"
+outputFile = simType + "February2014.csv"
 
 numAttributes = len(globalIncrease[0][0])
 def main():
-    T = [x for x in range(5,300,1)]
-    L = list(itertools.product(probPop, T, globalBalls, globalIncrease, discoveryRate, discoveryRateOptimal, recruitmentRate))
-    results = Parallel(n_jobs=-1, verbose=500000, pre_dispatch='all')(delayed(iteration)(cross, repetitions) for cross in L)    
-    
-    ### Write File ###
-    headers = 'probPopping,threshold,numChoices,share1,share2,time,noResolution,startingContents,increaseRates,discoveryRate,discoveryRateOptimal,recruitmentRate\n' 
     with open(outputFile, 'w') as f: 
+        headers = 'disruptionRateping,threshold,numChoices,option1,option2,time,noResolution,startingContents,increaseRates,discoveryRate,discoveryRateOptimal,recruitmentRate\n' 
         f.write(headers)
-        f.write('\n'.join('%s,%s,%s,%s,%s,%s,%s,"%s","%s",%s,%s,%s' % x for x in results))
+                
+    T = [x for x in range(1,100,1)]
+    L = list(itertools.product(disruptionRate, T, globalBalls, globalIncrease, discoveryRate, discoveryRateOptimal, recruitmentRate))
+    Parallel(n_jobs=-1, verbose=500000, pre_dispatch='all')(delayed(iteration)(cross, repetitions) for cross in L)
 
 def iteration(cross, repetitions):
     pid = win32api.GetCurrentProcessId()
     handle = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, True, pid)
     win32process.SetPriorityClass(handle, win32process.IDLE_PRIORITY_CLASS)
-    probPop, T, iterationBalls, iterationIncrease, discoveryRate, discoveryRateOptimal, recruitmentRate = cross
-    count, count2, time, noResolution = 0, 0, 0, 0
-
+    disruptionRate, T, iterationBalls, iterationIncrease, discoveryRate, discoveryRateOptimal, recruitmentRate = cross
+    output = []
     for i in range(repetitions):
         # Set up new iteration
         balls = list(iterationBalls)
         increase = list(iterationIncrease)
         numTargets = len(balls)
         # Run iteration
-        simulation = playGame(balls,T, increase, probPop, discoveryRate, discoveryRateOptimal, recruitmentRate)
+        simulation = playGame(balls,T, increase, disruptionRate, discoveryRate, discoveryRateOptimal, recruitmentRate)
         # Record Results
         if simulation["index"] == 0:
-            count +=1
-        if simulation["index"] == 1:
-            count2 += 1
-        time = time+simulation["time"]
-        noResolution += simulation["noResolution"]
-        if repetitions == noResolution:
-            avgTime = 0
+            option1 = 1
+            option2 = 0
+        elif simulation["index"] == 1:
+            option1 = 0
+            option2 = 1
         else:
-            avgTime = time/(repetitions - noResolution)
-    # Return results for output file
-    optimal = count/repetitions
-    optimal2 = count2/repetitions
-    return probPop, T, numTargets, optimal, optimal2, avgTime, noResolution, iterationBalls, iterationIncrease, discoveryRate, discoveryRateOptimal, recruitmentRate
-
+            option1, option2 = 0,0
+        time = simulation["time"]
+        if simulation["noResolution"] == 1:
+            option1 = 0
+            option2 = 0
+            time = 0
+        noResolution = simulation["noResolution"]
+        output.append([disruptionRate, T, numTargets, option1, option2, time, noResolution, iterationBalls, iterationIncrease, discoveryRate, discoveryRateOptimal, recruitmentRate])
+    writeToFile(output)
+    
 def drawBalls(balls, increase):
     ''' list -> int
         Takes as input a list with balls in the urn, 
@@ -81,31 +138,41 @@ def popBalls(balls):
     balls[index] = balls[index]-1
     return balls
 
-def playGame(balls, T, increase, probPop, discoveryRate, discoveryRateOptimal, recruitmentRate):
+def playGame(balls, T, increase, disruptionRate, discoveryRate, discoveryRateOptimal, recruitmentRate):
     time = 0
     count = 0
+    if discoveryRate == 0 and sum(balls) == 0:
+        return{"index":-88, "time":0, "noResolution":1}
     while max(balls) < T:
-        chooseAction = uniform(0, discoveryRate + sum(balls)*(probPop + recruitmentRate))
+        chooseAction = uniform(0, discoveryRate + sum(balls)*(disruptionRate + recruitmentRate))
         if chooseAction <= discoveryRate:
             if uniform(0,1) <= discoveryRateOptimal:
                 balls[0] += 1
             else:
                 balls[1] += 1         
-        elif chooseAction <= discoveryRate + sum(balls)*(probPop):
+        elif chooseAction <= discoveryRate + sum(balls)*(disruptionRate):
             if sum(balls) != 0:
                 balls = popBalls(balls)
-        elif chooseAction <= discoveryRate + sum(balls)*(probPop + recruitmentRate):
+        elif chooseAction <= discoveryRate + sum(balls)*(disruptionRate + recruitmentRate):
             if sum(balls) != 0:
                 balls = drawBalls(balls, increase)
-        time += incrementTime(balls, discoveryRate, recruitmentRate, probPop)
+        time += incrementTime(balls, discoveryRate, recruitmentRate, disruptionRate)
         count += 1
-        if count >= 100000:
+        if count >= 20000:
             return{"index":-99, "time":0, "noResolution":1}
     return{"index":balls.index(max(balls)), "time":time, "noResolution":0}
 
-def incrementTime(balls, discoveryRate, recruitmentRate, probPop):
-    increment = 1/(sum(balls)*(recruitmentRate + probPop) + discoveryRate)
+def incrementTime(balls, discoveryRate, recruitmentRate, disruptionRate):
+    increment = 1/(sum(balls)*(recruitmentRate + disruptionRate) + discoveryRate)
     return increment
+
+def writeToFile(data):
+    lock = lockfile.FileLock(outputFile)
+    lock.acquire()
+    with open(outputFile, 'a') as f:  
+        save = csv.writer(f, lineterminator='\n')
+        save.writerows(data)
+    lock.release()
 
 if __name__ == "__main__":
     main()
