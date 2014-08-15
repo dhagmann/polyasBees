cb6fd3637c0f0ac0870b5d9fb0ba153748712a2d
dhagmann <hagmann@cmu.edu>
2014-08-14 20:41:14 -0400

    Fixed Russell-time & new simulations
    Time calculated after drawing rather than before.
    Added new variance simulation
    Changed discovery simulation

--- Bees and Urns/polyaBees_discovery.py
+++ Bees and Urns/polyaBees_discovery.py
@@ -5,18 +5,32 @@ import csv, time
 import lockfile
 
 repetitions = 100000
-simType = "numTypes"
+simType = "variance"
 
+if simType == "newNoise":
+    globalBalls = ((1, 1,1),)
+    globalIncrease = (((3, 2, 2, 1),(2, 1, 1, 0),(2, 1, 1, 0)), ((2, 2, 2, 2),(1, 1, 1, 1), (1, 1, 1, 1)))
+    disruptionRate = [0,]
+    discoveryRate = [0,]
+    discoveryRateOptimal = [0,]
+    recruitmentRate = [1,]
+if simType == "newMultiAttributesDirectComparison":
+    globalBalls = ((1, 1),)
+    globalIncrease = (((4, 0), (2, 2)),)
+    disruptionRate = [0,]
+    discoveryRate = [0,]
+    discoveryRateOptimal = [0,]
+    recruitmentRate = [1,]
 if simType == "numTypes":
     globalBalls = ((1, 1), (1, 1, 1), (1, 1, 1, 1))
-    globalIncrease = (((2,),(1,), (1,), (1,)),)
+    globalIncrease = (((2,), (1,), (1,), (1,)),)
     disruptionRate = [0,]
     discoveryRate = [0,]
     discoveryRateOptimal = [0,]
     recruitmentRate = [1,]
 if simType == "ratios":
     globalBalls = ((1, 1, 1),)
-    globalIncrease = (((3,),(1,), (1,)), ((3,),(2,), (1,)))
+    globalIncrease = (((3,), (1,), (1,)), ((3,), (2,), (1,)))
     disruptionRate = [0,]
     discoveryRate = [0,]
     discoveryRateOptimal = [0,]
@@ -28,7 +42,7 @@ if simType == "noise":
     discoveryRate = [0,]
     discoveryRateOptimal = [0,]
     recruitmentRate = [1,]
-if simType == "multiAttributesFixed":
+if simType == "multiAttributes":
     globalBalls = ((1, 1),)
     globalIncrease = (((3, 1), (1, 1)),((2, 2), (1, 1)))
     disruptionRate = [0,]
@@ -66,20 +80,38 @@ if simType == "varyingOptimalPayoff":
 if simType == "discovery":
     globalBalls = ((0, 0),)
     globalIncrease = (((2,),(1,),),)
-    disruptionRate = [0.5]
-    discoveryRate = [0.25,0.75]
-    discoveryRateOptimal = [0.1,0.75]
-    recruitmentRate = [0.5,]
+    disruptionRate = [0, 0.05, 0.25]
+    discoveryRate = [0.25, 0.50, 1.0]
+    discoveryRateOptimal = [0.5,]
+    recruitmentRate = [0.25, 0.5, 1.0]
+if simType == "variance":
+    globalBalls = ((1, 1),)
+    # p * HIGH + (1-p)*1 == 2
+    # HIGH payoff varies, p is the probability that it is chosen
+    globalIncrease = (((1, 3), (2, 2)),
+                      ((1, 1, 4), (2, 2, 2)),
+                      ((1, 1, 1, 5), (2, 2, 2, 2)),
+                      ((1, 1, 1, 1, 6), (2, 2, 2, 2, 2)),
+                      ((1, 1, 1, 1, 1, 7), (2, 2, 2, 2, 2, 2)),
+                      ((1, 1, 1, 1, 1, 1, 8), (2, 2, 2, 2, 2, 2, 2)),
+                      ((1, 1, 1, 1, 1, 1, 1, 9), (2, 2, 2, 2, 2, 2, 2, 2)),
+                      ((1, 1, 1, 1, 1, 1, 1, 1, 10), (2, 2, 2, 2, 2, 2, 2, 2, 2)))
+    disruptionRate = [0,]
+    discoveryRate = [0,]
+    discoveryRateOptimal = [0,]
+    recruitmentRate = [1,]
 
-outputFile = simType + "February2014.csv"
+outputFile = simType + ".csv"
 
-numAttributes = len(globalIncrease[0][0])
+# numAttributes = len(globalIncrease[0][0])
 def main():
     with open(outputFile, 'w') as f: 
-        headers = 'disruptionRateping,threshold,numChoices,option1,option2,time,noResolution,startingContents,increaseRates,discoveryRate,discoveryRateOptimal,recruitmentRate\n' 
+        headers = 'disruptionRate,threshold,numChoices,error,option1,option2,time,noResolution,startingContents,increaseRates,discoveryRate,discoveryRateOptimal,recruitmentRate\n' 
         f.write(headers)
                 
-    T = [x for x in range(1,100,1)]
+    T = [x for x in range(2,100,1)] # + [x for x in range(100, 5001, 100)]
+    if simType == "variance":
+        T = [100,]
     L = list(itertools.product(disruptionRate, T, globalBalls, globalIncrease, discoveryRate, discoveryRateOptimal, recruitmentRate))
     Parallel(n_jobs=-1, verbose=500000, pre_dispatch='all')(delayed(iteration)(cross, repetitions) for cross in L)
 
@@ -111,7 +143,8 @@ def iteration(cross, repetitions):
             option2 = 0
             time = 0
         noResolution = simulation["noResolution"]
-        output.append([disruptionRate, T, numTargets, option1, option2, time, noResolution, iterationBalls, iterationIncrease, discoveryRate, discoveryRateOptimal, recruitmentRate])
+        error = 1-option1
+        output.append([disruptionRate, T, numTargets, error, option1, option2, time, noResolution, iterationBalls, iterationIncrease, discoveryRate, discoveryRateOptimal, recruitmentRate])
     writeToFile(output)
     
 def drawBalls(balls, increase):
@@ -119,6 +152,7 @@ def drawBalls(balls, increase):
         Takes as input a list with balls in the urn, 
         then returns a new list with one additional ball.
     '''
+    numAttributes = len(increase[0])
     rnd = randint(1, sum(balls))
     index = 0
     while sum(balls[0:index+1]) < rnd:
@@ -141,24 +175,27 @@ def popBalls(balls):
 def playGame(balls, T, increase, disruptionRate, discoveryRate, discoveryRateOptimal, recruitmentRate):
     time = 0
     count = 0
-    if discoveryRate == 0 and sum(balls) == 0:
-        return{"index":-88, "time":0, "noResolution":1}
     while max(balls) < T:
+        time += incrementTime(balls, discoveryRate, recruitmentRate, disruptionRate)
         chooseAction = uniform(0, discoveryRate + sum(balls)*(disruptionRate + recruitmentRate))
-        if chooseAction <= discoveryRate:
-            if uniform(0,1) <= discoveryRateOptimal:
+        if chooseAction < discoveryRate:
+            if uniform(0,1) < discoveryRateOptimal:
                 balls[0] += 1
             else:
                 balls[1] += 1         
-        elif chooseAction <= discoveryRate + sum(balls)*(disruptionRate):
+        elif chooseAction < discoveryRate + sum(balls)*(disruptionRate):
             if sum(balls) != 0:
                 balls = popBalls(balls)
-        elif chooseAction <= discoveryRate + sum(balls)*(disruptionRate + recruitmentRate):
+        elif chooseAction < discoveryRate + sum(balls)*(disruptionRate + recruitmentRate):
             if sum(balls) != 0:
                 balls = drawBalls(balls, increase)
-        time += incrementTime(balls, discoveryRate, recruitmentRate, disruptionRate)
+        else:
+            print("ERROR: playGame function")
+
+        if discoveryRate == 0 and sum(balls) == 0:
+            return{"index":-88, "time":0, "noResolution":1}
         count += 1
-        if count >= 20000:
+        if count >= 100000000:
             return{"index":-99, "time":0, "noResolution":1}
     return{"index":balls.index(max(balls)), "time":time, "noResolution":0}
 
